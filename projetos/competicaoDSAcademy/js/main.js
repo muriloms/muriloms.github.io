114ea7acf04cf1a7133a34413b1f96f91deea038
Murilo Mazzotti Silvestrini <murilo.silvestrini@mapsolutions.com>
2019-09-07 08:46:20 -0300

    up

--- projetos/competicaoDSAcademy/js/main.js
+++ projetos/competicaoDSAcademy/js/main.js
@@ -1,54 +1,59 @@
 
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#plot1")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+function plotBar(){
+  // set the dimensions and margins of the graph
+  var margin = {top: 30, right: 30, bottom: 70, left: 60},
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
 
-// get the data
-d3.csv("https://muriloms.github.io/data/1_OneNum.csv", function(data) {
+  // append the svg object to the body of the page
+  var svg = d3.select("#plot1")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-  // X axis: scale and draw:
-  var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg.append("g")
+  // Parse the Data
+  d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+
+    // sort data
+    data.sort(function(b, a) {
+      return a.Value - b.Value;
+    });
+
+    // X axis
+    var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.Personagem; }))
+      .padding(0.2);
+    svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
 
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return d.price; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
-
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
-
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg.append("g")
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, 13000])
+      .range([ height, 0]);
+    svg.append("g")
       .call(d3.axisLeft(y));
 
-  // append the bar rectangles to the svg element
-  svg.selectAll("rect")
-      .data(bins)
+    // Bars
+    svg.selectAll("mybar")
+      .data(data)
       .enter()
       .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
+        .attr("x", function(d) { return x(d.Personagem); })
+        .attr("y", function(d) { return y(d.Value); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.Value); })
+        .attr("fill", "#69b3a2")
 
-});
+  })
+
+}
+plotBar();

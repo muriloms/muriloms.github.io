8dd3268050db0f340ebade9af52f9fbd16fbcfc8
Murilo Mazzotti Silvestrini <murilo.silvestrini@mapsolutions.com>
2019-09-07 08:09:28 -0300

    corrigir bug jquery

--- projetos/competicaoDSAcademy/js/main.js
+++ projetos/competicaoDSAcademy/js/main.js
@@ -14,7 +14,7 @@ var svg = d3.select("#plot1")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
+d3.csv("https://muriloms.github.io/data/1_OneNum.csv", function(data) {
 
   // X axis: scale and draw:
   var x = d3.scaleLinear()

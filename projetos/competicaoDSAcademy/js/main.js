a89813035c354ec82171efb94920819809a3d6f7
Murilo Mazzotti Silvestrini <murilo.silvestrini@mapsolutions.com>
2019-09-07 10:55:38 -0300

    up

--- projetos/competicaoDSAcademy/js/main.js
+++ projetos/competicaoDSAcademy/js/main.js
@@ -16,7 +16,7 @@ function plotBar(){
             "translate(" + margin.left + "," + margin.top + ")");
 
   // Parse the Data
-  d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+  d3.csv("https://muriloms.github.io/data/dados1.csv", function(data) {
 
     // sort data
     data.sort(function(b, a) {
@@ -37,7 +37,7 @@ function plotBar(){
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([0, 13000])
+      .domain([0, 31])
       .range([ height, 0]);
     svg.append("g")
       .call(d3.axisLeft(y));
@@ -51,9 +51,122 @@ function plotBar(){
         .attr("y", function(d) { return y(d.Value); })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d.Value); })
-        .attr("fill", "#69b3a2")
+        .attr("fill", "#CC5418")
 
   })
 
 }
 plotBar();
+
+
+function plot2(){
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 30, bottom: 30, left: 30},
+      width = 500 - margin.left - margin.right,
+      height = 180 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  var svg = d3.select("#plot3")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  //Read the data
+  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv", function(data) {
+
+      // List of groups (here I have one group per column)
+      var allGroup = ["valueA", "valueB", "valueC"]
+
+      // add the options to the button
+      d3.select("#selectButton")
+        .selectAll('myOptions')
+        .data(allGroup)
+        .enter()
+        .append('option')
+        .text(function (d) { return d; }) // text showed in the menu
+        .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+      // A color scale: one color for each group
+      var myColor = d3.scaleOrdinal()
+        .domain(allGroup)
+        .range(['#0C12FF', '#CC5418', 'black']);
+
+      // Add X axis --> it is a date format
+      var x = d3.scaleLinear()
+        .domain([0,10])
+        .range([ 0, width ]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain( [0,20])
+        .range([ height, 0 ]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
+
+      // Initialize line with group a
+      var line = svg
+        .append('g')
+        .append("path")
+          .datum(data)
+          .attr("d", d3.line()
+            .x(function(d) { return x(+d.time) })
+            .y(function(d) { return y(+d.valueA) })
+          )
+          .attr("stroke", function(d){ return myColor("valueA") })
+          .style("stroke-width", 4)
+          .style("fill", "none")
+
+      // A function that update the chart
+      function update(selectedGroup) {
+
+        // Create new data with the selection?
+        var dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+
+        // Give these new data to update line
+        line
+            .datum(dataFilter)
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+              .x(function(d) { return x(+d.time) })
+              .y(function(d) { return y(+d.value) })
+            )
+            .attr("stroke", function(d){ return myColor(selectedGroup) })
+      }
+
+      // When the button is changed, run the updateChart function
+      d3.select("#selectButton").on("change", function(d) {
+          // recover the option that has been chosen
+          var selectedOption = d3.select(this).property("value")
+          // run the updateChart function with this selected option
+          update(selectedOption)
+      })
+
+  })
+}
+plot2();
+
+function plot3(){
+
+
+  var data = [{
+    values: [19, 26, 55],
+    labels: ['Residential', 'Non-Residential', 'Utility'],
+    type: 'pie'
+  }];
+
+  var layout = {
+    height: 350,
+    width: 400
+  };
+
+  Plotly.newPlot('plot3', data, layout);
+
+}
+//plot3();
